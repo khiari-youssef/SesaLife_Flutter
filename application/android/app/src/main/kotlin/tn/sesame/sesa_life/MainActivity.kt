fc867eb218a22f04741a6b40b9e43bfdca68a6b1
Youssef Khiari <khiari.youssef98@gmail.com>
2024-05-23 13:37:39 +0100

    task(platform): android permissions + activity update

--- application/android/app/src/main/kotlin/tn/sesame/sesa_life/MainActivity.kt
+++ application/android/app/src/main/kotlin/tn/sesame/sesa_life/MainActivity.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity(){
+class MainActivity: FlutterFragmentActivity(){
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState, persistentState)
