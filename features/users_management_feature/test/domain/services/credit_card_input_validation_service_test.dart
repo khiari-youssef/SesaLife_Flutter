5a533faaecd3e9b0626cb0c14fb9ff8a7d56f6eb
Youssef Khiari <khiari.youssef98@gmail.com>
2024-07-12 15:31:47 +0100

    + unit test + refactoring

--- features/users_management_feature/test/domain/services/credit_card_input_validation_service_test.dart
+++ features/users_management_feature/test/domain/services/credit_card_input_validation_service_test.dart
@@ -4,79 +4,92 @@ import 'package:users_management_feature/domain/services/credit_card_input_valid
 void main() {
   CreditCardInputValidationService creditCardInputValidationService =
       CreditCardInputValidationService();
-  test("InvalidCreditCardNumberLength", () {
-    expect(
-        creditCardInputValidationService
-            .isCreditCardNumberValid("4526 5674 775"),
-        false);
-  });
-  test("InvalidCreditCardNumberLength", () {
-    expect(
-        creditCardInputValidationService.isCreditCardNumberValid("  "), false);
-  });
-  test("InvalidCreditCardNumberFormatWithLetters", () {
-    expect(
-        creditCardInputValidationService
-            .isCreditCardNumberValid("4526 5674 7756 5DX4"),
-        false);
-  });
-  test("InvalidCreditCardNumberNetwork", () {
-    expect(
-        creditCardInputValidationService
-            .isCreditCardNumberValid("2526 5674 7756 7476"),
-        false);
-  });
-  test("ValidCreditCardNumber", () {
-    expect(
-        creditCardInputValidationService
-            .isCreditCardNumberValid("4526 5674 7756 7476"),
-        true);
-  });
-
-  test("UnValidCCExpiryDateWithBlankValue", () {
-    expect(creditCardInputValidationService.isCCExpiryDateValid(" "), false);
+  group("CreditCardNumberInputTestGroup", () {
+    test("InvalidCreditCardNumberLength", () {
+      expect(
+          creditCardInputValidationService
+              .isCreditCardNumberValid("4526 5674 775"),
+          false);
+    });
+    test("InvalidCreditCardNumberLength", () {
+      expect(creditCardInputValidationService.isCreditCardNumberValid("  "),
+          false);
+    });
+    test("InvalidCreditCardNumberFormatWithLetters", () {
+      expect(
+          creditCardInputValidationService
+              .isCreditCardNumberValid("4526 5674 7756 5DX4"),
+          false);
+    });
+    test("InvalidCreditCardNumberNetwork", () {
+      expect(
+          creditCardInputValidationService
+              .isCreditCardNumberValid("2526 5674 7756 7476"),
+          false);
+    });
+    test("ValidCreditCardNumber", () {
+      expect(
+          creditCardInputValidationService
+              .isCreditCardNumberValid("4526 5674 7756 7476"),
+          true);
+    });
   });
 
-  test("UnValidCCExpiryDateWithIsoDateFormat", () {
-    expect(creditCardInputValidationService.isCCExpiryDateValid("02-07-2024"),
-        false);
+  group("CCExpiryDateInputTestGroup", () {
+    test("UnValidCCExpiryDateWithBlankValue", () {
+      expect(creditCardInputValidationService.isCCExpiryDateValid(" "), false);
+    });
+
+    test("UnValidCCExpiryDateWithIsoDateFormat", () {
+      expect(creditCardInputValidationService.isCCExpiryDateValid("02-07-2024"),
+          false);
+    });
+    test("UnValidCCExpiryDateWithInvalidDateFormat", () {
+      expect(creditCardInputValidationService.isCCExpiryDateValid("02/07/2024"),
+          false);
+    });
+    test("UnValidCCExpiryDateWithLetters", () {
+      expect(
+          creditCardInputValidationService.isCCExpiryDateValid("ab/5v"), false);
+    });
+    test("ValidCCExpiryDate", () {
+      expect(
+          creditCardInputValidationService.isCCExpiryDateValid("09/29"), true);
+    });
   });
-  test("UnValidCCExpiryDateWithInvalidDateFormat", () {
-    expect(creditCardInputValidationService.isCCExpiryDateValid("02/07/2024"),
-        false);
+
+  group("CCHolderNameInputTestGroup", () {
+    test("UnValidCCholderNameWithBlankValue", () {
+      expect(creditCardInputValidationService.isCCHolderNameDValid(" "), false);
+    });
+    test("UnValidCCholderNameWithDigitLetters", () {
+      expect(
+          creditCardInputValidationService.isCCHolderNameDValid("AB45"), false);
+    });
+    test("ValidCCholderName", () {
+      expect(
+          creditCardInputValidationService
+              .isCCHolderNameDValid("KHIARI YOUSSEF"),
+          true);
+    });
   });
-  test("UnValidCCExpiryDateWithLetters", () {
-    expect(
-        creditCardInputValidationService.isCCExpiryDateValid("ab/5v"), false);
-  });
-  test("ValidCCExpiryDate", () {
-    expect(creditCardInputValidationService.isCCExpiryDateValid("09/29"), true);
-  });
-  test("UnValidCCholderNameWithBlankValue", () {
-    expect(creditCardInputValidationService.isCCHolderNameDValid(" "), false);
-  });
-  test("UnValidCCholderNameWithDigitLetters", () {
-    expect(
-        creditCardInputValidationService.isCCHolderNameDValid("AB45"), false);
-  });
-  test("ValidCCholderName", () {
-    expect(
-        creditCardInputValidationService.isCCHolderNameDValid("KHIARI YOUSSEF"),
-        true);
-  });
-  test("UnValidCVVcodeWithEmptyValue", () {
-    expect(creditCardInputValidationService.isCVValid(" "), false);
-  });
-  test("UnValidCVVcodeWithInCorrectLengthAndLetterChars", () {
-    expect(creditCardInputValidationService.isCVValid("KHIARI YOUSSEF"), false);
-  });
-  test("UnValidCVVcodeWithInCorrectLengthWithDigitChars", () {
-    expect(creditCardInputValidationService.isCVValid("3563"), false);
-  });
-  test("UnValidCVVcodeWithCorrectLengthAndContainsNonDigitChars", () {
-    expect(creditCardInputValidationService.isCVValid("ABC"), false);
-  });
-  test("ValidCVVcode", () {
-    expect(creditCardInputValidationService.isCVValid("353"), true);
+
+  group("CCcVVInputTestGroup", () {
+    test("UnValidCVVcodeWithEmptyValue", () {
+      expect(creditCardInputValidationService.isCVValid(" "), false);
+    });
+    test("UnValidCVVcodeWithInCorrectLengthAndLetterChars", () {
+      expect(
+          creditCardInputValidationService.isCVValid("KHIARI YOUSSEF"), false);
+    });
+    test("UnValidCVVcodeWithInCorrectLengthWithDigitChars", () {
+      expect(creditCardInputValidationService.isCVValid("3563"), false);
+    });
+    test("UnValidCVVcodeWithCorrectLengthAndContainsNonDigitChars", () {
+      expect(creditCardInputValidationService.isCVValid("ABC"), false);
+    });
+    test("ValidCVVcode", () {
+      expect(creditCardInputValidationService.isCVValid("353"), true);
+    });
   });
 }
