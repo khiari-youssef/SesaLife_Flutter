ffa9a6307ea681304980704a4753bd178eb23c69
Youssef Khiari <khiari.youssef98@gmail.com>
2024-07-15 11:32:19 +0100

    + refactoring

---
+++ features/users_management_feature/lib/ui/enrollment/step2/EnrollmentStep2PageStateDataState.freezed.dart
@@ -0,0 +1,253 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'EnrollmentStep2PageStateDataState.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$EnrollmentStep2PageStateDataState {
+  FormTextValidationState get firstNameState =>
+      throw _privateConstructorUsedError;
+  int? get selectedSexIndex => throw _privateConstructorUsedError;
+  FormTextValidationState get birthDateState =>
+      throw _privateConstructorUsedError;
+  FormTextValidationState get diplomaState =>
+      throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $EnrollmentStep2PageStateDataStateCopyWith<EnrollmentStep2PageStateDataState>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $EnrollmentStep2PageStateDataStateCopyWith<$Res> {
+  factory $EnrollmentStep2PageStateDataStateCopyWith(
+          EnrollmentStep2PageStateDataState value,
+          $Res Function(EnrollmentStep2PageStateDataState) then) =
+      _$EnrollmentStep2PageStateDataStateCopyWithImpl<$Res,
+          EnrollmentStep2PageStateDataState>;
+  @useResult
+  $Res call(
+      {FormTextValidationState firstNameState,
+      int? selectedSexIndex,
+      FormTextValidationState birthDateState,
+      FormTextValidationState diplomaState});
+
+  $FormTextValidationStateCopyWith<$Res> get firstNameState;
+  $FormTextValidationStateCopyWith<$Res> get birthDateState;
+  $FormTextValidationStateCopyWith<$Res> get diplomaState;
+}
+
+/// @nodoc
+class _$EnrollmentStep2PageStateDataStateCopyWithImpl<$Res,
+        $Val extends EnrollmentStep2PageStateDataState>
+    implements $EnrollmentStep2PageStateDataStateCopyWith<$Res> {
+  _$EnrollmentStep2PageStateDataStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? firstNameState = null,
+    Object? selectedSexIndex = freezed,
+    Object? birthDateState = null,
+    Object? diplomaState = null,
+  }) {
+    return _then(_value.copyWith(
+      firstNameState: null == firstNameState
+          ? _value.firstNameState
+          : firstNameState // ignore: cast_nullable_to_non_nullable
+              as FormTextValidationState,
+      selectedSexIndex: freezed == selectedSexIndex
+          ? _value.selectedSexIndex
+          : selectedSexIndex // ignore: cast_nullable_to_non_nullable
+              as int?,
+      birthDateState: null == birthDateState
+          ? _value.birthDateState
+          : birthDateState // ignore: cast_nullable_to_non_nullable
+              as FormTextValidationState,
+      diplomaState: null == diplomaState
+          ? _value.diplomaState
+          : diplomaState // ignore: cast_nullable_to_non_nullable
+              as FormTextValidationState,
+    ) as $Val);
+  }
+
+  @override
+  @pragma('vm:prefer-inline')
+  $FormTextValidationStateCopyWith<$Res> get firstNameState {
+    return $FormTextValidationStateCopyWith<$Res>(_value.firstNameState,
+        (value) {
+      return _then(_value.copyWith(firstNameState: value) as $Val);
+    });
+  }
+
+  @override
+  @pragma('vm:prefer-inline')
+  $FormTextValidationStateCopyWith<$Res> get birthDateState {
+    return $FormTextValidationStateCopyWith<$Res>(_value.birthDateState,
+        (value) {
+      return _then(_value.copyWith(birthDateState: value) as $Val);
+    });
+  }
+
+  @override
+  @pragma('vm:prefer-inline')
+  $FormTextValidationStateCopyWith<$Res> get diplomaState {
+    return $FormTextValidationStateCopyWith<$Res>(_value.diplomaState, (value) {
+      return _then(_value.copyWith(diplomaState: value) as $Val);
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$EnrollmentStep2PageStateDataStateImplCopyWith<$Res>
+    implements $EnrollmentStep2PageStateDataStateCopyWith<$Res> {
+  factory _$$EnrollmentStep2PageStateDataStateImplCopyWith(
+          _$EnrollmentStep2PageStateDataStateImpl value,
+          $Res Function(_$EnrollmentStep2PageStateDataStateImpl) then) =
+      __$$EnrollmentStep2PageStateDataStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {FormTextValidationState firstNameState,
+      int? selectedSexIndex,
+      FormTextValidationState birthDateState,
+      FormTextValidationState diplomaState});
+
+  @override
+  $FormTextValidationStateCopyWith<$Res> get firstNameState;
+  @override
+  $FormTextValidationStateCopyWith<$Res> get birthDateState;
+  @override
+  $FormTextValidationStateCopyWith<$Res> get diplomaState;
+}
+
+/// @nodoc
+class __$$EnrollmentStep2PageStateDataStateImplCopyWithImpl<$Res>
+    extends _$EnrollmentStep2PageStateDataStateCopyWithImpl<$Res,
+        _$EnrollmentStep2PageStateDataStateImpl>
+    implements _$$EnrollmentStep2PageStateDataStateImplCopyWith<$Res> {
+  __$$EnrollmentStep2PageStateDataStateImplCopyWithImpl(
+      _$EnrollmentStep2PageStateDataStateImpl _value,
+      $Res Function(_$EnrollmentStep2PageStateDataStateImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? firstNameState = null,
+    Object? selectedSexIndex = freezed,
+    Object? birthDateState = null,
+    Object? diplomaState = null,
+  }) {
+    return _then(_$EnrollmentStep2PageStateDataStateImpl(
+      firstNameState: null == firstNameState
+          ? _value.firstNameState
+          : firstNameState // ignore: cast_nullable_to_non_nullable
+              as FormTextValidationState,
+      selectedSexIndex: freezed == selectedSexIndex
+          ? _value.selectedSexIndex
+          : selectedSexIndex // ignore: cast_nullable_to_non_nullable
+              as int?,
+      birthDateState: null == birthDateState
+          ? _value.birthDateState
+          : birthDateState // ignore: cast_nullable_to_non_nullable
+              as FormTextValidationState,
+      diplomaState: null == diplomaState
+          ? _value.diplomaState
+          : diplomaState // ignore: cast_nullable_to_non_nullable
+              as FormTextValidationState,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$EnrollmentStep2PageStateDataStateImpl
+    implements _EnrollmentStep2PageStateDataState {
+  _$EnrollmentStep2PageStateDataStateImpl(
+      {required this.firstNameState,
+      this.selectedSexIndex,
+      required this.birthDateState,
+      required this.diplomaState});
+
+  @override
+  final FormTextValidationState firstNameState;
+  @override
+  final int? selectedSexIndex;
+  @override
+  final FormTextValidationState birthDateState;
+  @override
+  final FormTextValidationState diplomaState;
+
+  @override
+  String toString() {
+    return 'EnrollmentStep2PageStateDataState(firstNameState: $firstNameState, selectedSexIndex: $selectedSexIndex, birthDateState: $birthDateState, diplomaState: $diplomaState)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$EnrollmentStep2PageStateDataStateImpl &&
+            (identical(other.firstNameState, firstNameState) ||
+                other.firstNameState == firstNameState) &&
+            (identical(other.selectedSexIndex, selectedSexIndex) ||
+                other.selectedSexIndex == selectedSexIndex) &&
+            (identical(other.birthDateState, birthDateState) ||
+                other.birthDateState == birthDateState) &&
+            (identical(other.diplomaState, diplomaState) ||
+                other.diplomaState == diplomaState));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, firstNameState, selectedSexIndex,
+      birthDateState, diplomaState);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$EnrollmentStep2PageStateDataStateImplCopyWith<
+          _$EnrollmentStep2PageStateDataStateImpl>
+      get copyWith => __$$EnrollmentStep2PageStateDataStateImplCopyWithImpl<
+          _$EnrollmentStep2PageStateDataStateImpl>(this, _$identity);
+}
+
+abstract class _EnrollmentStep2PageStateDataState
+    implements EnrollmentStep2PageStateDataState {
+  factory _EnrollmentStep2PageStateDataState(
+          {required final FormTextValidationState firstNameState,
+          final int? selectedSexIndex,
+          required final FormTextValidationState birthDateState,
+          required final FormTextValidationState diplomaState}) =
+      _$EnrollmentStep2PageStateDataStateImpl;
+
+  @override
+  FormTextValidationState get firstNameState;
+  @override
+  int? get selectedSexIndex;
+  @override
+  FormTextValidationState get birthDateState;
+  @override
+  FormTextValidationState get diplomaState;
+  @override
+  @JsonKey(ignore: true)
+  _$$EnrollmentStep2PageStateDataStateImplCopyWith<
+          _$EnrollmentStep2PageStateDataStateImpl>
+      get copyWith => throw _privateConstructorUsedError;
+}
