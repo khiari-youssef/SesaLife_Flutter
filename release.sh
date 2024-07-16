bafc8ffe16bf97f9bd6f776e5dfaa176a6fe8297
Youssef Khiari <khiari.youssef98@gmail.com>
2024-07-16 14:55:40 +0100

    + CI shell scripts

---
+++ release.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+
+echo "${BLUE} Build task :"
+#sh build.sh || (echo "${RED} CI build task failed" && exit)
+echo "${BLUE} Test Task :"
+cd test-runner || exit
+flutter test test/testAppRunner_test.dart || (echo "${RED} CI test task failed" && exit)
+cd ..
+echo "${BLUE} Release Task :"
+flutter clean
+echo "${GREEN} Android app bundle generation Task :"
+cd application || exit
+OBFUSCATION_DEBUG_OUTPUT_DIR="$(pwd)/build/app/outputs/obfuscation"
+flutter build appbundle --release --obfuscate --split-debug-info="$OBFUSCATION_DEBUG_OUTPUT_DIR"  || (echo "${RED} CI Android AppBundle build task failed" && exit)
+echo "${GREEN} iOs app package generation Task :"
+flutter build ipa --obfuscate --split-debug-info="$OBFUSCATION_DEBUG_OUTPUT_DIR" || (echo "${RED} CI iOs AppBundle build task failed" && exit)
+
--- test-runner/runner.sh
+++ test-runner/runner.sh
@@ -2,10 +2,11 @@
 
 GREEN='\033[0;32m'
 # shellcheck disable=SC2039
+
 echo "${GREEN} Initializing application for tests..."
 flutter clean
 flutter pub get
 flutter pub run build_runner build --delete-conflicting-outputs
 echo "${GREEN} Running application  tests..."
-flutter test test/testAppRunner_test.dart
+
 echo "${BLUE} test task completed"
