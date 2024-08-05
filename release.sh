aa32d1b1f16e485c6b2c8a9f105d077832cdaa9d
Youssef Khiari <khiari.youssef98@gmail.com>
2024-08-05 12:55:34 +0100

    task(github actions workflow updated): + java setup + script updated

--- release.sh
+++ release.sh
@@ -8,9 +8,9 @@ BLUE='\033[0;34m'
 echo "${BLUE} Release Task :"
 flutter clean
 echo "${GREEN} Android app bundle generation Task :"
-cd application || exit
+cd application || exit 125
 OBFUSCATION_DEBUG_OUTPUT_DIR="$(pwd)/build/app/outputs/obfuscation"
-flutter build appbundle --release --obfuscate --split-debug-info="$OBFUSCATION_DEBUG_OUTPUT_DIR"  || (echo "${RED} CI Android AppBundle build task failed" && exit)
+flutter build appbundle --release --obfuscate --split-debug-info="$OBFUSCATION_DEBUG_OUTPUT_DIR"  || (echo "${RED} CI Android AppBundle build task failed" && exit 125)
 # echo "${GREEN} iOs app package generation Task :"
 #flutter build ipa --obfuscate --split-debug-info="$OBFUSCATION_DEBUG_OUTPUT_DIR" || (echo "${RED} CI iOs AppBundle build task failed" && exit)
 
