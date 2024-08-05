b64c382b96a70da48fc650cc85761ed197b13f65
Youssef Khiari <khiari.youssef98@gmail.com>
2024-08-05 14:47:51 +0100

    task(users-management): + lowercased project files

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:shared_dependencies/shared_dependencies.dart';
-import 'package:users_management_feature/infrastructure/dataSources/users_hive_boxes_init.dart';
+import 'package:users_management_feature/infrastructure/data_sources//users_hive_boxes_init.dart';
 import 'package:users_management_feature/infrastructure/dtos/hive_adapters_register.dart';
 
 import 'app_navigation_configuration.dart';
