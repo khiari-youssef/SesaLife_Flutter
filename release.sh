56aa404e406cffac81ad19cb684a279961e76540
Youssef Khiari <khiari.youssef98@gmail.com>
2024-08-05 12:31:30 +0100

    task(github actions workflow updated): + gradle setup action + separated the flutter test cmds from the release script file.

--- release.sh
+++ release.sh
@@ -4,12 +4,6 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 
-echo "${BLUE} Build task :"
-sh build.sh || (echo "${RED} CI build task failed" && exit)
-echo "${BLUE} Test Task :"
-cd test-runner || exit
-flutter test test/testAppRunner_test.dart || (echo "${RED} CI test task failed" && exit)
-cd ..
 echo "${BLUE} Release Task :"
 flutter clean
 echo "${GREEN} Android app bundle generation Task :"
